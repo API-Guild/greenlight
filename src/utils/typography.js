6ea6cd6d2490a7c4b778a6b906eda733e00e4cd8
Stephen Price <stephen_lprice@live.com>
2021-04-09 14:05:42 -0500

    installed and configured typograhy with ocean beach theme

---
+++ src/utils/typography.js
@@ -0,0 +1,7 @@
+import Typography from "typography"
+import oceanBeachTheme from 'typography-theme-ocean-beach'
+
+const typography = new Typography(oceanBeachTheme)
+
+export const { scale, rhythm, options } = typography
+export default typography
