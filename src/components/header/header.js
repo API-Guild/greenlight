631be36a90613971b0dd27562d5c6631987f07bf
Stephen Price <stephen_lprice@live.com>
2021-04-10 05:50:44 -0500

    adding dark theme background colors to global.scss

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,4 +1,5 @@
 import React from "react"
+import "../../pages/global.scss"
 
 export default function Header() {
   return (
--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -1,4 +1,5 @@
 import React from "react"
+import "../../pages/global.scss"
 import Header from "../header/header"
 
 
