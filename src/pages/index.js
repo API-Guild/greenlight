bad346a3d57ae1dd6785de3e180b48ba659dbda6
Stephen Price <stephen_lprice@live.com>
2021-04-10 02:44:20 -0500

    installed bulma, added brand colors and darktheme. Created README.md

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,29 @@
 import React from "react"
+import "../styles/global.scss"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div>
+      <section class="hero">
+        <div class="hero-body">
+          <p class="title">
+            Greenlight
+          </p>
+          <p class="subtitle">
+            A guerrilla <strong>data portal</strong> for people with deadlines.
+          </p>
+        </div>
+      </section>
+      <section class="section">
+        <div class="container">
+          <div class="notification is-primary">
+            <h1 class="title">Section</h1>
+            <h2 class="subtitle">
+              A simple container to divide your page into <strong>sections</strong>, like the one you're currently reading.
+            </h2>
+          </div>
+        </div>
+      </section>
+    </div>
+  )
 }
