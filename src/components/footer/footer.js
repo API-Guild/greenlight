32917cbad4640d212072774046a31ab7d49ce719
Stephen Price <stephen_lprice@live.com>
2021-04-12 04:36:38 -0500

    modified footer text

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -6,9 +6,8 @@ export default function Footer() {
         <div className="container">
           <div className="content has-text-centered">
             <p>
-              <strong>Bulma</strong> by <a href="https://jgthms.com">Jeremy Thomas</a>. The source code is licensed
-              <a href="http://opensource.org/licenses/mit-license.php"> MIT</a>. The website content
-              is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
+              <strong>Greenlight</strong> by <a href="https://stephenlprice.github.io/portfolio/index.html">Stephen Price</a> and <a href="https://trevorsmithbanjo.github.io/#/">Trevor Smith</a>. The source code is licensed
+              <a href="https://opensource.org/licenses/0BSD"> 0BSD</a>. Built with <a href="https://www.gatsbyjs.com/">Gatsby</a> and <a href="https://bulma.io/">Bulma</a>.
             </p>
           </div>
         </div>
