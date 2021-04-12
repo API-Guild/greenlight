654504a614a85692de95f73972b725ff2c7418bb
Stephen Price <stephen_lprice@live.com>
2021-04-12 05:05:19 -0500

    added blank targets to footer links

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -6,8 +6,8 @@ export default function Footer() {
         <div className="container">
           <div className="content has-text-centered">
             <p>
-              <strong>Greenlight</strong> by <a href="https://stephenlprice.github.io/portfolio/index.html">Stephen Price</a> and <a href="https://trevorsmithbanjo.github.io/#/">Trevor Smith</a>. The source code is licensed
-              <a href="https://opensource.org/licenses/0BSD"> 0BSD</a>. Built with <a href="https://www.gatsbyjs.com/">Gatsby</a> and <a href="https://bulma.io/">Bulma</a>.
+              <strong>Greenlight</strong> by <a href="https://stephenlprice.github.io/portfolio/index.html" target="_blank">Stephen Price</a> and <a href="https://trevorsmithbanjo.github.io/#/" target="_blank">Trevor Smith</a>. The source code is licensed
+              <a href="https://opensource.org/licenses/0BSD" target="_blank"> 0BSD</a>. Built with <a href="https://www.gatsbyjs.com/" target="_blank">Gatsby</a> and <a href="https://bulma.io/" target="_blank">Bulma</a>.
             </p>
           </div>
         </div>
