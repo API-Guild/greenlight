09b29772e8f8785ccfcddc3389e3190a87b75770
Stephen Price <stephen_lprice@live.com>
2021-04-12 00:36:09 -0500

    resized hero title and subtitle

--- src/components/hero/hero.js
+++ src/components/hero/hero.js
@@ -4,10 +4,10 @@ export default function Hero() {
   return (
     <section className="section">
       <div className="container is-justify-content-flex-start">
-        <p className="title has-text-primary">
+        <p className="title has-text-primary is-1">
           Greenlight
         </p>
-        <p className="subtitle">
+        <p className="subtitle is-4">
           A guerrilla <strong>data portal</strong> for people with deadlines.
         </p>
       </div>
