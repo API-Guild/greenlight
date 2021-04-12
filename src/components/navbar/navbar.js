0dade6db3f80c51418d10e4bda085af467a3129f
Trevor Smith <trevorsmithbanjo@gmail.com>
2021-04-12 17:04:29 -0500

    Links MD Files To Navbar.

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,26 +1,56 @@
 import React from "react"
 import greenlight from "../../assets/svg/greenlight.svg"
+import { graphql, Link, useStaticQuery } from "gatsby"
 
 export default function Header() {
-
   const showMenu = () => {
-    const menu = document.getElementById('navMenu');
-    const burger = document.getElementById('burger');
+    const menu = document.getElementById("navMenu")
+    const burger = document.getElementById("burger")
 
-    menu.classList.toggle('is-active');
-    burger.classList.toggle('is-active');
-  };
+    menu.classList.toggle("is-active")
+    burger.classList.toggle("is-active")
+  }
+
+  // Gatsby hook for graphql queries that aren't page components
+  const data = useStaticQuery(
+    graphql`
+      query {
+        allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+          edges {
+            node {
+              id
+              frontmatter {
+                title
+              }
+              fields {
+                slug
+              }
+            }
+          }
+        }
+      }
+    `
+  )
 
   return (
     <nav className="navbar" role="navigation" aria-label="main navigation">
       <div className="navbar-brand">
         <a className="navbar-item" href="/">
-          <img className="logo" src={greenlight} alt="logo"/>
+          <img className="logo" src={greenlight} alt="logo" />
           Greenlight
         </a>
 
-        <span id='burger' role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" 
-        data-target="navMenu" onClick={showMenu} onKeyUp={showMenu} tabIndex={0}>
+        <span
+          id="burger"
+          role="button"
+          className="navbar-burger"
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navMenu"
+          onClick={showMenu}
+          onKeyUp={showMenu}
+          tabIndex={0}
+        >
           <span aria-hidden="true" data-target="navMenu"></span>
           <span aria-hidden="true" data-target="navMenu"></span>
           <span aria-hidden="true" data-target="navMenu"></span>
@@ -29,21 +59,36 @@ export default function Header() {
 
       <div id="navMenu" className="navbar-menu">
         <div className="navbar-start">
-          <a className="navbar-item" href="/about">About</a>
-          <a className="navbar-item" href="/contact">Contact</a>
+          <a className="navbar-item" href="/about">
+            About
+          </a>
+          <a className="navbar-item" href="/contact">
+            Contact
+          </a>
 
           <div className="navbar-item has-dropdown is-hoverable">
-            <a className="navbar-link" href="/reports">Reports</a>
+            <a className="navbar-link" href="/reports">
+              Reports
+            </a>
 
             <div className="navbar-dropdown">
-              <a className="navbar-item" href="/">Financials</a>
+              {/* <a className="navbar-item" href="/">Financials</a>
               <a className="navbar-item" href="/">Supply Chain</a>
               <a className="navbar-item" href="/">Sales</a>
               <a className="navbar-item" href="/">QA/QC</a>
               <a className="navbar-item" href="/">Marketing</a>
-              <a className="navbar-item" href="/">Human Resources</a>
+              <a className="navbar-item" href="/">Human Resources</a> */}
+              {data.allMarkdownRemark.edges.map(({ node }) => (
+                <Link to={node.fields.slug}>
+                  <a className="navbar-item" key={node.id}>
+                    {node.frontmatter.title}
+                  </a>
+                </Link>
+              ))}
               <hr className="navbar-divider"></hr>
-              <a className="navbar-item" href="/">Explore</a>
+              <a className="navbar-item" href="/">
+                Explore
+              </a>
             </div>
           </div>
         </div>
@@ -54,9 +99,7 @@ export default function Header() {
               <button className="button is-primary">
                 <strong>Sign up</strong>
               </button>
-              <button className="button is-light">
-                Log in
-              </button>
+              <button className="button is-light">Log in</button>
             </div>
           </div>
         </div>
--- src/templates/data-table-post.js
+++ src/templates/data-table-post.js
@@ -6,21 +6,21 @@ export default function BlogPost({ data }) {
   const post = data.markdownRemark
   return (
     <Layout>
-      {/* <div>
+      <div>
         <h1>{post.frontmatter.title}</h1>
         <div dangerouslySetInnerHTML={{ __html: post.html }} />
-      </div> */}
+      </div>
     </Layout>
   )
 }
 
-// export const query = graphql`
-//   query($slug: String!) {
-//     markdownRemark(fields: { slug: { eq: $slug } }) {
-//       html
-//       frontmatter {
-//         title
-//       }
-//     }
-//   }
-// `
+export const query = graphql`
+  query($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      html
+      frontmatter {
+        title
+      }
+    }
+  }
+`
