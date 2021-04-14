bfc5ee4eeb12ddd22bb1d9f31f93af448ae55ab8
Trevor Smith <trevorsmithbanjo@gmail.com>
2021-04-14 17:54:30 -0500

    H1's Apply Bulma's is-size-1 Class Usine rehype-react.

---
+++ src/components/bulmaBasics/isSize1.js
@@ -0,0 +1,5 @@
+import React from "react"
+
+export default function isSize1(props) {
+  return <h1 className="is-size-1">{props.children}</h1>
+}
--- src/templates/post-template.js
+++ src/templates/post-template.js
@@ -2,17 +2,31 @@ import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout/layout"
 import Hero from "../components/hero/hero"
+import rehypeReact from "rehype-react"
+import isSize1 from "../components/bulmaBasics/isSize1"
 
 export default function BlogPost({ data }) {
-  const post = data.markdownRemark;
-  const frontmatter = post.frontmatter;
+  const post = data.markdownRemark
+  const frontmatter = post.frontmatter
+
+  const renderAst = new rehypeReact({
+    createElement: React.createElement,
+    components: {
+      h1: isSize1,
+    },
+  }).Compiler
 
   return (
     <Layout>
-      <Hero title={frontmatter.title} subtitle={frontmatter.description} date={frontmatter.date} />
-        <div className="container is-fluid">
-          <div dangerouslySetInnerHTML={{ __html: post.html }} />
-        </div>
+      <Hero
+        title={frontmatter.title}
+        subtitle={frontmatter.description}
+        date={frontmatter.date}
+      />
+      <div className="container is-fluid">
+        {/* <div dangerouslySetInnerHTML={{ __html: post.html }} /> */}
+        {renderAst(post.htmlAst)}
+      </div>
     </Layout>
   )
 }
@@ -20,10 +34,10 @@ export default function BlogPost({ data }) {
 export const query = graphql`
   query($slug: String!) {
     markdownRemark(fields: { slug: { eq: $slug } }) {
-      html
+      htmlAst
       frontmatter {
-        title,
-        description,
+        title
+        description
         date
       }
     }
