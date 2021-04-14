00240c26e848e6cd32f7940ee9827785fdec50ec
Stephen Price <stephen_lprice@live.com>
2021-04-14 01:45:08 -0500

    added an SEO component with react helment that will allow for calling on Tableau JS API

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -1,16 +1,20 @@
 import React from "react"
 import Navbar from "../navbar/navbar"
 import Footer from "../footer/footer"
+import Seo from "../seo/seo"
 
 
 export default function Layout({ children }) {
   return (
-    <div className="site">
-      <Navbar/>
-      <main className="main">
-        {children}
-      </main>
-      <Footer/>
-    </div>
+    <>
+      <Seo />
+      <div className="site">
+        <Navbar/>
+        <main className="main">
+          {children}
+        </main>
+        <Footer/>
+      </div>
+    </>
   )
 }
