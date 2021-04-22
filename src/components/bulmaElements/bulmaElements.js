fab67bc6ef8fffa43bf88cc0956e5776df82955b
Trevor Smith <trevorsmithbanjo@gmail.com>
2021-04-22 17:02:02 -0500

    Adds Bulma Heading Styles with MDXProvider.

---
+++ src/components/bulmaElements/bulmaElements.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export const isSize1 = props => <h1 className="is-size-1" {...props} />
+export const isSize2 = props => <h2 className="is-size-2" {...props} />
+export const isSize3 = props => <h3 className="is-size-3" {...props} />
+export const isSize4 = props => <h4 className="is-size-4" {...props} />
+export const isSize5 = props => <h5 className="is-size-5" {...props} />
+export const isSize6 = props => <h6 className="is-size-6" {...props} />
+export const isSize7 = props => <h7 className="is-size-7" {...props} />
--- src/templates/post-template.js
+++ src/templates/post-template.js
@@ -4,6 +4,16 @@ import { graphql } from "gatsby"
 import Layout from "../components/layout/layout"
 import Hero from "../components/hero/hero"
 import { MDXRenderer } from "gatsby-plugin-mdx"
+import { MDXProvider } from "@mdx-js/react"
+import {
+  isSize1,
+  isSize2,
+  isSize3,
+  isSize4,
+  isSize5,
+  isSize6,
+  isSize7,
+} from "../components/bulmaElements/bulmaElements"
 
 export default function BlogPost({ data }) {
   const post = data.mdx
@@ -12,8 +22,19 @@ export default function BlogPost({ data }) {
   const fullPath = meta.tableauServer + pathSegment + meta.tableauVersion
   const viz = meta.viz
 
+  // const isSize1 = props => <h1 className="is-size-1" {...props} />
+  const components = {
+    h1: isSize1,
+    h2: isSize2,
+    h3: isSize3,
+    h4: isSize4,
+    h5: isSize5,
+    h6: isSize6,
+    h7: isSize7,
+  }
+
   return (
-    <>
+    <h1>
       <Helmet>
         {meta.tableauServer && meta.tableauVersion ? (
           <script type="text/javascript" src={fullPath} />
@@ -21,11 +42,13 @@ export default function BlogPost({ data }) {
       </Helmet>
       <Layout>
         <Hero title={meta.title} subtitle={meta.description} date={meta.date} />
-        <div className="container is-fluid content">
-          <MDXRenderer>{post.body}</MDXRenderer>
+        <div className="container is-fluid">
+          <MDXProvider components={components}>
+            <MDXRenderer>{post.body}</MDXRenderer>
+          </MDXProvider>
         </div>
       </Layout>
-    </>
+    </h1>
   )
 }
 
