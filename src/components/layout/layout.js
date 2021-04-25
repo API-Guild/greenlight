0b9808df8d53e1d39827f6767fca2933af99ad7e
Stephen Price <stephen_lprice@live.com>
2021-04-25 03:25:46 -0500

    fixed tableau embed bug

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -1,5 +1,4 @@
 import React from "react"
-import Helmet from "react-helmet"
 import { MDXProvider } from "@mdx-js/react"
 import Navbar from "../navbar/navbar"
 import Footer from "../footer/footer"
@@ -8,15 +7,11 @@ import Callout from "../callout/callout"
 import Tableau from "../tableau/tableau"
 
 const shortcodes = { Callout, Tableau };
-const tableauApi = "https://public.tableau.com/javascripts/api/tableau-2.7.0.min.js";
 
 export default function Layout({ children }) {
   return (
     <>
       <Seo />
-      <Helmet>
-        <script type="text/javascript" src={tableauApi} />
-      </Helmet>
       <div className="site">
         <Navbar />
         <main className="main">
