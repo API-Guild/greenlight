e3ae6dc03ac24cd297b102c38eed98ac97611a94
Stephen Price <stephen_lprice@live.com>
2021-04-27 23:22:05 -0500

    .mdx supports inline images

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,6 +29,27 @@ module.exports = {
   },
   /* Your site config here */
   plugins: [
+    `gatsby-plugin-sharp`,
+    `gatsby-remark-images`,
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        gatsbyRemarkPlugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 1200,
+            },
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/blog/`,
+      },
+    },
     `gatsby-plugin-sass`,
     {
       resolve: `gatsby-plugin-purgecss`,
