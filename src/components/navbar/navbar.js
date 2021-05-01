1af4603f0a4db7cedcaa8d8bd1348ccdfa72f1c0
Stephen Price <stephen_lprice@live.com>
2021-05-01 10:58:19 -0500

    adding explore elements

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -86,6 +86,17 @@ export default function Navbar() {
               <Link className="navbar-item" to="/dashboards">
                 Explore
               </Link>
+              <div class="field">
+                <p class="control has-icons-left has-icons-right">
+                  <input class="input" type="email" placeholder="Email"/>
+                  <span class="icon is-small is-left">
+                    <i class="fas fa-envelope"></i>
+                  </span>
+                  <span class="icon is-small is-right">
+                    <i class="fas fa-check"></i>
+                  </span>
+                </p>
+              </div>
             </div>
           </div>
         </div>
