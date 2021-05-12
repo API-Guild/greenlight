029dfbfdc543928b5e5b344595d8555a0482616d
Trevor Smith <trevorsmithbanjo@gmail.com>
2021-05-12 17:26:22 -0500

    Adds Social Media Functionality to New Seo Component.

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -2,11 +2,11 @@ import React from "react"
 import { MDXProvider } from "@mdx-js/react"
 import Navbar from "../navbar/navbar"
 import Footer from "../footer/footer"
-import Seo from "../seo/seo"
+import Seo from "../seo/newSeo"
 import Callout from "../callout/callout"
 import Tableau from "../tableau/tableau"
 
-const shortcodes = { Callout, Tableau };
+const shortcodes = { Callout, Tableau }
 
 export default function Layout({ children }) {
   return (
