9b625df51ca9dffe757a08226c568b3c8a5de5d9
Stephen Price <stephen_lprice@live.com>
2021-06-08 15:44:49 -0500

    added a reusable logo component

---
+++ src/components/logo/logo.js
@@ -0,0 +1,10 @@
+import React from "react"
+import { ReactComponent as Greenlight} from "../../assets/svg/greenlight.svg"
+
+// this component is meant to receive an .svg file as a logo
+// for dynamic sizing and coloring
+export default function Logo() {
+  return (
+    <Greenlight />
+  )
+}
