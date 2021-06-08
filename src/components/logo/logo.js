2e0bc496e23212e22d6dbcba253df8ef1eed3fc6
Stephen Price <stephen_lprice@live.com>
2021-06-08 16:07:40 -0500

    added a module.css file for the logo

--- src/components/logo/logo.js
+++ src/components/logo/logo.js
@@ -1,10 +1,11 @@
 import React from "react"
 import { ReactComponent as Greenlight} from "../../assets/svg/greenlight.svg"
+import * as logoStyles from "./logo.module.css"
 
 // this component is meant to receive an .svg file as a logo
 // for dynamic sizing and coloring
 export default function Logo() {
   return (
-    <Greenlight />
+    <Greenlight className={logoStyles.logo}/>
   )
 }
