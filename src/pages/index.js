23cbc588dfd61e4d80c0b2994c7b6c612a2ce6e2
Stephen Price <stephen_lprice@live.com>
2021-06-09 13:13:54 -0500

    first draft of homepage content

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { Link } from "gatsby"
 import Layout from "../components/layout/layout"
 import Hero from "../components/hero/hero"
 import Cartoon from "../components/cartoon/cartoon"
@@ -10,7 +11,7 @@ import { ReactComponent as StackedAreaChart } from "../assets/svg/stackedAreaCha
 import { ReactComponent as Builds } from "../assets/svg/builds.svg"
 import { ReactComponent as Security } from "../assets/svg/security.svg"
 import { ReactComponent as Pwa } from "../assets/svg/pwa.svg"
-import { ReactComponent as PredictiveAnalytics } from "../assets/svg/chilling.svg"
+import { ReactComponent as Chilling } from "../assets/svg/chilling.svg"
 
 export default function Home() {
   return (
@@ -26,166 +27,183 @@ export default function Home() {
         <PostChart/>
       </Cartoon>
       <section className="section">
-        <p className="has-text-centered has-text-left-mobile is-size-5-mobile is-size-3-tablet">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore 
-          et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut 
-          aliquip ex ea commodo consequat.
+        <p className="has-text-centered has-text-primary has-text-left-mobile is-size-5-mobile is-size-3-tablet">
+          The <strong>Tableau</strong> blog you've always wanted!
+        </p>
+        <p className="has-text-centered has-text-left-mobile is-size-6-mobile is-size-5-tablet">
+          Leverage the modern web to build a site that's fast, secure
+          and low maintenance. Forget about deploying servers and dealing with malware. You don't need hundreds 
+          of hours of work to learn these techniques either. Instead, focus on your content and let
+          <Link to="/"> Greenlight</Link> make everything easier.
         </p>
       </section>
 
       <section className="section">
         {/* columns for tablets and above */}
         <div className="columns is-hidden-mobile">
-          <Box>
-            <div className="columns">
-              <div className="column is-flex is-align-self-center">
-                <Cartoon>
-                  <StackedAreaChart/>
-                </Cartoon>
+          <div className="column is-half is-flex">
+            <Box>
+              <div className="columns">
+                <div className="column is-flex is-align-self-center">
+                  <Cartoon>
+                    <StackedAreaChart/>
+                  </Cartoon>
+                </div>
+                <div className="column is-flex is-align-self-center">
+                  <Title
+                    title="Embedded Analytics"
+                    titleColor="primary"
+                    titleSize={4}
+                    subtitle={
+                      <p>
+                        has never been easier. Adding dashboards to your blog is almost as 
+                        straightforward as <i className="has-text-primary">'drag & drop'</i>. 
+                        More like, embed and forget. It just works.
+                      </p>
+                    }
+                    subtitleColor="grey lighter"
+                    subtitleSize={6}
+                  />
+                </div>
               </div>
-              <div className="column is-flex is-align-self-center">
-                <Title
-                  title="Embedded Analytics"
-                  titleColor="primary"
-                  titleSize={4}
-                  subtitle={
-                    <p>
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore 
-                      et dolore magna aliqua.
-                    </p>
-                  }
-                  subtitleColor="grey lighter"
-                  subtitleSize={6}
-                />
+            </Box>
+          </div>
+          <div className="column is-half is-flex">
+            <Box>
+              <div className="columns">
+                <div className="column is-flex is-align-self-center">
+                  <Cartoon>
+                    <Articles/>
+                  </Cartoon>
+                </div>
+                <div className="column is-flex is-align-self-center">
+                  <Title
+                    title="Compelling Content"
+                    titleColor="primary"
+                    titleSize={4}
+                    subtitle={
+                      <p>
+                        is empowered by data. Help the world see what matters to you.
+                        Knowledge is power and it's measured in data.
+                      </p>
+                    }
+                    subtitleColor="grey lighter"
+                    subtitleSize={6}
+                  />
+                </div>
               </div>
-            </div>
-          </Box>
-          <Box>
-            <div className="columns">
-              <div className="column is-flex is-align-self-center">
-                <Cartoon>
-                  <Articles/>
-                </Cartoon>
-              </div>
-              <div className="column is-flex is-align-self-center">
-                <Title
-                  title="Compelling Content"
-                  titleColor="primary"
-                  titleSize={4}
-                  subtitle={
-                    <p>
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore 
-                      et dolore magna aliqua.
-                    </p>
-                  }
-                  subtitleColor="grey lighter"
-                  subtitleSize={6}
-                />
-              </div>
-            </div>
-          </Box>
+            </Box> 
+          </div>
         </div>
 
         <div className="columns is-hidden-mobile">
-          <Box>
-            <div className="columns">
-              <div className="column is-flex is-align-self-center">
-                <Title
-                  title="Static Site Generator"
-                  titleColor="primary"
-                  titleSize={4}
-                  subtitle={
-                    <p>
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore 
-                      et dolore magna aliqua.
-                    </p>
-                  }
-                  subtitleColor="grey lighter"
-                  subtitleSize={6}
-                />
+          <div className="column is-half is-flex">
+            <Box>
+              <div className="columns">
+                <div className="column is-flex is-align-self-center">
+                  <Title
+                    title="Static Site Generator"
+                    titleColor="primary"
+                    titleSize={4}
+                    subtitle={
+                      <p>
+                        Create a dynamic and optimized website using a modern stack that frees 
+                        you from deploying and maintaining servers.
+                      </p>
+                    }
+                    subtitleColor="grey lighter"
+                    subtitleSize={6}
+                  />
+                </div>
+                <div className="column is-flex is-align-self-center">
+                  <Cartoon>
+                    <Builds/>
+                  </Cartoon>
+                </div>
               </div>
-              <div className="column is-flex is-align-self-center">
-                <Cartoon>
-                  <Builds/>
-                </Cartoon>
+            </Box>
+          </div>
+          <div className="column is-half is-flex">
+            <Box>
+              <div className="columns">
+                <div className="column is-flex is-align-self-center">
+                  <Title
+                    title="Secure"
+                    titleColor="primary"
+                    titleSize={4}
+                    subtitle={
+                      <p>
+                        Going serverless means that you can forget about malware, malicious requests & DDOS
+                        attacks. It also makes hosting much cheaper.
+                      </p>
+                    }
+                    subtitleColor="grey lighter"
+                    subtitleSize={6}
+                  />
+                </div>
+                <div className="column is-flex is-align-self-center">
+                  <Cartoon>
+                    <Security/>
+                  </Cartoon>
+                </div>
               </div>
-            </div>
-          </Box>
-          <Box>
-            <div className="columns">
-              <div className="column is-flex is-align-self-center">
-                <Title
-                  title="Secure"
-                  titleColor="primary"
-                  titleSize={4}
-                  subtitle={
-                    <p>
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore 
-                      et dolore magna aliqua.
-                    </p>
-                  }
-                  subtitleColor="grey lighter"
-                  subtitleSize={6}
-                />
-              </div>
-              <div className="column is-flex is-align-self-center">
-                <Cartoon>
-                  <Security/>
-                </Cartoon>
-              </div>
-            </div>
-          </Box>
+            </Box>
+          </div>
         </div>
 
         <div className="columns is-hidden-mobile">
-          <Box>
-            <div className="columns">
-              <div className="column is-flex is-align-self-center">
-                <Cartoon>
-                  <Pwa/>
-                </Cartoon>
+          <div className="column is-half is-flex">
+            <Box>
+              <div className="columns">
+                <div className="column is-flex is-align-self-center">
+                  <Cartoon>
+                    <Pwa/>
+                  </Cartoon>
+                </div>
+                <div className="column is-flex is-align-self-center">
+                  <Title
+                    title="Responsive UI"
+                    titleColor="primary"
+                    titleSize={4}
+                    subtitle={
+                      <p>
+                        Readers can enjoy your blog on any device. Everything from charts
+                        down to text is responsive.
+                      </p>
+                    }
+                    subtitleColor="grey lighter"
+                    subtitleSize={6}
+                  />
+                </div>
               </div>
-              <div className="column is-flex is-align-self-center">
-                <Title
-                  title="Responsive UI"
-                  titleColor="primary"
-                  titleSize={4}
-                  subtitle={
-                    <p>
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore 
-                      et dolore magna aliqua.
-                    </p>
-                  }
-                  subtitleColor="grey lighter"
-                  subtitleSize={6}
-                />
+            </Box>
+          </div>
+          <div className="column is-half is-flex">
+            <Box>
+              <div className="columns">
+                <div className="column is-flex is-align-self-center">
+                  <Cartoon>
+                    <Chilling/>
+                  </Cartoon>
+                </div>
+                <div className="column is-flex is-align-self-center">
+                  <Title
+                    title="Stress Free"
+                    titleColor="primary"
+                    titleSize={4}
+                    subtitle={
+                      <p>
+                        Follow the <Link to="/blog/quick-start">Quick Start</Link> guide. Host the site for
+                        cheap. Forget about servers. Dashboards just work.
+                      </p>
+                    }
+                    subtitleColor="grey lighter"
+                    subtitleSize={6}
+                  />
+                </div>
               </div>
-            </div>
-          </Box>
-          <Box>
-            <div className="columns">
-              <div className="column is-flex is-align-self-center">
-                <Cartoon>
-                  <PredictiveAnalytics/>
-                </Cartoon>
-              </div>
-              <div className="column is-flex is-align-self-center">
-                <Title
-                  title="Stress Free"
-                  titleColor="primary"
-                  titleSize={4}
-                  subtitle={
-                    <p>
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore 
-                      et dolore magna aliqua.
-                    </p>
-                  }
-                  subtitleColor="grey lighter"
-                  subtitleSize={6}
-                />
-              </div>
-            </div>
-          </Box>
+            </Box>
+          </div>
         </div>
 
         {/* columns for mobile */}
@@ -320,7 +338,7 @@ export default function Home() {
             <div className="columns">
               <div className="column is-flex is-align-self-center">
                 <Cartoon>
-                  <PredictiveAnalytics/>
+                  <Chilling/>
                 </Cartoon>
               </div>
               <div className="column is-flex is-align-self-center">
