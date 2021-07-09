56826e81936ffe9a1e6b8539f96fec436f0c513a
Stephen Price <stephen_lprice@live.com>
2021-07-08 23:28:17 -0500

    replaced inline link svg with an import

--- src/components/bulmaElements/bulmaElements.js
+++ src/components/bulmaElements/bulmaElements.js
@@ -1,4 +1,6 @@
 import React from "react"
+import { ReactComponent as AnchorLink } from "../../assets/svg/anchorLink.svg"
+
 
 export const isSize1 = props => (
   <h1 
@@ -11,10 +13,7 @@ export const isSize1 = props => (
       aria-label={props.children} 
       className="anchor"
     >
-      <svg aria-hidden="true" height="20" version="1.1" viewBox="0 0 16 16" width="20">
-        <path fill-rule="evenodd" d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z">
-        </path>
-      </svg>
+      <AnchorLink/>
     </a>
     {props.children}
   </h1>
@@ -31,10 +30,7 @@ export const isSize2 = props => (
       aria-label={props.children} 
       className="anchor"
     >
-      <svg aria-hidden="true" height="20" version="1.1" viewBox="0 0 16 16" width="20">
-        <path fill-rule="evenodd" d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z">
-        </path>
-      </svg>
+      <AnchorLink/>
     </a>
     {props.children}
   </h2>
@@ -51,10 +47,7 @@ export const isSize3 = props => (
       aria-label={props.children} 
       className="anchor"
     >
-      <svg aria-hidden="true" height="20" version="1.1" viewBox="0 0 16 16" width="20">
-        <path fill-rule="evenodd" d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z">
-        </path>
-      </svg>
+      <AnchorLink/>
     </a>
     {props.children}
   </h3>
