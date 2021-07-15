0ad0e82fa58743e8ea829d3da108c2982bfb7fe8
Stephen Price <stephen_lprice@live.com>
2021-07-15 16:11:13 -0500

    refactored vizNav.js to use layout context

--- src/components/vizToolbar/components/vizNav/vizNav.js
+++ src/components/vizToolbar/components/vizNav/vizNav.js
@@ -1,9 +1,13 @@
-import React from "react"
+import React, { useContext } from 'react'
+import classNames from 'classnames'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBackward, faForward } from '@fortawesome/free-solid-svg-icons'
+import LayoutContext from '../../../../context/LayoutContext'
 import { navBtn, vizNav } from "./vizNav.module.css"
 
 export default function VizNav(props) {
+  const { width } = useContext(LayoutContext);
+
   const btnStyles = `${props.color} ${props.outline} ${props.rounded} ${navBtn}`;
   const indexBtn = `${props.color} ${props.outline} ${props.rounded}`;
   const vizzes = props.vizUrl.length;
@@ -18,44 +22,29 @@ export default function VizNav(props) {
     props.handleVizIndex(-1);
   }
 
+  const buttonsDivClass = classNames({
+    'buttons is-centered': true,
+    'are-small': width < 540,
+    [`${vizNav}`]: true,
+  });
+
   return (
-    <>
-      <div className={`buttons are-small is-centered is-hidden-tablet ${vizNav}`}>
-        {/* mobile layout */}
-        <button className={`button ${btnStyles}`} onClick={Prev}>
-          <span className="icon">
-            <FontAwesomeIcon icon={faBackward}/>
-          </span>
-          <span><strong>Previous</strong></span>
-        </button>
+    <div className={buttonsDivClass}>
+      <button className={`button ${btnStyles}`} onClick={Prev}>
+        <span className="icon">
+          <FontAwesomeIcon icon={faBackward}/>
+        </span>
+        <span><strong>Previous</strong></span>
+      </button>
 
-        <button className={`button ${indexBtn}`}>{props.vizIndex + 1}/{vizzes}</button>
+      <button className={`button ${indexBtn}`}>{props.vizIndex + 1}/{vizzes}</button>
 
-        <button className={`button ${btnStyles}`} onClick={Next}>
-          <span><strong>Next</strong></span>
-          <span className="icon">
-            <FontAwesomeIcon icon={faForward}/>
-          </span>
-        </button>
-      </div>
-      {/* desktop layout */}
-      <div className={`buttons is-centered is-hidden-mobile ${vizNav}`}>
-        <button className={`button ${btnStyles}`} onClick={Prev}>
-          <span className="icon">
-            <FontAwesomeIcon icon={faBackward}/>
-          </span>
-          <span><strong>Previous</strong></span>
-        </button>
-
-        <button className={`button ${indexBtn}`}>{props.vizIndex + 1}/{vizzes}</button>
-
-        <button className={`button ${btnStyles}`} onClick={Next}>
-          <span><strong>Next</strong></span>
-          <span className="icon">
-            <FontAwesomeIcon icon={faForward}/>
-          </span>
-        </button>
-      </div>
-    </>
+      <button className={`button ${btnStyles}`} onClick={Next}>
+        <span><strong>Next</strong></span>
+        <span className="icon">
+          <FontAwesomeIcon icon={faForward}/>
+        </span>
+      </button>
+    </div>
   )
 }
