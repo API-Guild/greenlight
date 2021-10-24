9f55076bbe195c84b87f65082c6327afe05875ca
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2021-10-24 18:44:43 -0500

    refactored the cartoon.js file to handle SVG displays on safari and chrome correctly

--- src/components/cartoon/cartoon.js
+++ src/components/cartoon/cartoon.js
@@ -1,10 +1,11 @@
 import React from "react"
+import { cartoon } from "./cartoon.module.scss"
 
 export default function Cartoon(props) {
 
   return (
     <div className="container">
-      <figure className="image cartoon is-flex is-justify-content-center">
+      <figure className={`image cartoon is-flex is-justify-content-center ${cartoon}`}>
         {props.children}
       </figure>
     </div>
