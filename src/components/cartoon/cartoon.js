197953edbb796ac449e4946241552de6c56b6cd4
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2021-10-24 22:40:49 -0500

    refactored cartoon component for flexible configuration via props, now supports floats

--- src/components/cartoon/cartoon.js
+++ src/components/cartoon/cartoon.js
@@ -1,10 +1,15 @@
 import React from "react"
-import { cartoon } from "./cartoon.module.scss"
+import classNames from "classnames"
+import { cartoon, left, right } from "./cartoon.module.scss"
 
 export default function Cartoon(props) {
+  const container = classNames({
+    [`${left}`]: props.float === 'left',
+    [`${right}`]: props.float === 'right',
+  });
 
   return (
-    <div className="container">
+    <div className={`container ${props.classes} ${container}`} style={{width: props.width, margin: props.margin}}>
       <figure className={`image cartoon is-flex is-justify-content-center ${cartoon}`}>
         {props.children}
       </figure>
