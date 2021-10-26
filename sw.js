a87010dcd91bcb66896cb8aea0bbef04a138edf2
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2021-10-25 23:09:08 -0500

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-2057adf9129c2fb1dc2e.js"
+    "url": "webpack-runtime-1eb3721bca9c08bd7eb7.js"
   },
   {
     "url": "styles.080b318738408c23e921.css"
@@ -40,7 +40,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "39a85da64bfb2d6728c1a668928cea4d"
+    "revision": "732dde9e7158d5c974230f724eb96176"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-24c6229b14d138291071.js"
@@ -51,7 +51,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "c2d91e525a40d6831cf18c224a071e48"
+    "revision": "5c4f50cc4efa5803e97af54aecf88579"
   },
   {
     "url": "polyfill-59cc4c511920fb19b85a.js"
